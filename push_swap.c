52741254de2a4e9a87995f3108369124eeffb17a
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-11-28 14:38:16 +0100

    validinput

--- push_swap.c
+++ push_swap.c
@@ -6,25 +6,31 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 11:10:25 by fsitter           #+#    #+#             */
-/*   Updated: 2025/11/28 14:10:01 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/11/28 14:38:06 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
 int	main(void)
 {
-	char *s = "+-500";
-	
-	int atoi_s = ft_atoi(s);
+	char	*av[] = {"20", "5+", "hello", "6", "3", "4", NULL};
+	int		ac;
 
-	ft_printf("%i\n", atoi_s);
-	ft_printf("strlen original:\t%i\n", ft_strlen(s));
-	ft_printf("strlen itoa of atoi:\t%i\n", ft_strlen(ft_itoa(atoi_s)));
-	
+	ac = 1;
+	while (av[ac - 1])
+		ac++;
 	
 }
 
+int	valid_input(char* av, int *ac)
+{
+	int len_of_av_i;
+	int len_of_itoa_of_atoi_of_av_i;
+	
+	
+}
 
 // int	main(void)
 // {
@@ -32,14 +38,14 @@ int	main(void)
 // 	t_stack stack_b;
 
 // 	char *numbers[] = {"20", "5+", "hello", "6", "3", "4", NULL};
-	
+
 // 	int ac = 1;
-	
+
 // 	while (numbers[ac-1])
 // 		ac++;
 // 	ft_printf("%i\n", ac);
 // 	int i = 0;
-	
+
 // 	while (numbers[i])
 // 	{
 // 		ft_printf("%i\n", ft_atoi(numbers[i]));
