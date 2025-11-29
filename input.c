f152c65473e20f185e5f6fb323ebb1db2ad346bd
forkroyal <gfabits12@gmail.com>
2025-11-29 15:02:03 +0100

    eine line./s +1

--- input.c
+++ input.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 13:03:48 by fsitter           #+#    #+#             */
-/*   Updated: 2025/11/29 14:29:14 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/11/29 15:00:49 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ int	valid_integers_plus(char **av, int ac)
 	int		i;
 	int		j;
 
-	if (!ft_strncmp(av[1], "-0", ft_strlen(av[1])))
-		return (1);
 	i = 1;
 	while (i < ac)
 	{
+		if (!ft_strncmp(av[i], "-0", ft_strlen(av[1])))
+			i++;
+		if (!av[i])
+			return (1);
 		itoa_of_atoi = ft_itoa(ft_atoi(av[i]));
 		j = 0;
-		while (av[i][j])
+		while (av[i] && av[i][j])
 		{
 			if ((av[i][j] == itoa_of_atoi[j]) || (ft_strchr("+", av[i][0]))
 				&& av[i][j + 1] == itoa_of_atoi[j]
