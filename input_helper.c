17fd05bd7080fedbd2acfa119db660079089af6f
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-12-01 18:07:26 +0100

    av=2

--- input_helper.c
+++ input_helper.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 13:03:48 by fsitter           #+#    #+#             */
-/*   Updated: 2025/12/01 10:27:00 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/12/01 18:03:58 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,6 @@ int	f_valid_integers_plus(char **av, int ac)
 		if (!ft_strncmp(av[i], "-0", ft_strlen(av[i])))
 			i++;
 		else if (!ft_strncmp(av[i], itoa_of_atoi, ft_strlen(av[i])))
-			i++;
-		else if (!ft_strncmp(av[i], "+", ft_strlen(av[i])))
-			return(free(itoa_of_atoi), 0);
-		else if ((ft_strchr("+", av[i][0])) && !(ft_strncmp(av[i]+1, itoa_of_atoi, (ft_strlen(av[i])-1))))
 			i++;
 		else
 			return(free(itoa_of_atoi), 0);
--- sandbox/valid_integers.c
+++ sandbox/valid_integers.c
@@ -1,4 +1,5 @@
 int	valid_integers(char **av, int ac);
+int	f_valid_integers_plus(char **av, int ac);
 
 int	valid_integers(char **av, int ac)
 {
@@ -23,5 +24,31 @@ int	valid_integers(char **av, int ac)
 		free(itoa_of_atoi);
 		i++;
 	}
+	return (1);
+}
+
+int	f_valid_integers_plus(char **av, int ac)
+{
+	char	*itoa_of_atoi;
+	int		i;
+
+	i = 1;
+	while (i < ac)
+	{
+		itoa_of_atoi = ft_itoa(ft_atoi(av[i]));
+		if (!ft_strncmp(av[i], "-0", ft_strlen(av[i])))
+			i++;
+		else if (!ft_strncmp(av[i], itoa_of_atoi, ft_strlen(av[i])))
+			i++;
+		else if (!ft_strncmp(av[i], "+", ft_strlen(av[i])))
+			return(free(itoa_of_atoi), 0);
+		else if ((ft_strchr("+", av[i][0])) && !(ft_strncmp(av[i]+1, itoa_of_atoi, (ft_strlen(av[i])-1))))
+			i++;
+		else
+			return(free(itoa_of_atoi), 0);
+		free(itoa_of_atoi);
+	}
+	if (f_find_duplicate(av, ac))
+		return (0);
 	return (1);
 }
