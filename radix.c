8948f1a18e55bb61b66dded437952a89b710e577
Your Name <you@example.com>
2025-12-04 02:00:37 +0100

    nobattery

--- radix.c
+++ radix.c
@@ -13,14 +13,20 @@ int f_max_runs(int max_index)
 void    f_radix(t_stack *stack_a, t_stack *stack_b)
 {
     int max_runs;
+    int size;
     int i;
 
     i = 0;
+    size = stack_a->size;
     max_runs = f_max_runs(f_find_max_index(stack_a));
     while (max_runs > i)
     {
-        if (stack_a->top->index >> i == 0)
-            
+        j = 0;
+        while (j < size)
+        {
+            if (stack_a->top->index >> i == 0)
+                
+
 
     }
 
